a009f3c5a264b28c976c39922e01c51a1a2e929c
murisly <murisly2015@gmail.com>
2016-08-25 16:29:14 +0800

    constant

---
+++ views/site/constant.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: macalyou
+ * Date: 2016/7/7
+ * Time: 23:18
+ */
+use yii\helpers\Html;
+
+?>
+
+<div>
+    <div>dir:<?= $dir ?></div>
+    <div>method:<?= $method ?></div>
+    <div>file:<?= $file ?></div>
+    <div>class:<?= $class ?></div>
+</div>
