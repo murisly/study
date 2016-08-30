9712158e851c60ae762527be283166cad95836df
murisly <murisly2015@gmail.com>
2016-08-30 22:30:06 +0800

    add css

---
+++ controllers/CssController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace app\controllers;
+
+use yii\web\Controller;
+
+class CssController extends Controller
+{
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+
+    public function actionOuter() {
+        return $this->render('html/outer.html');
+    }
+}
+
